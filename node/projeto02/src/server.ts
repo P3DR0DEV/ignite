fa32db91a3c5beacaabb1a43d982e4bb0aeef202
Pedro Mendes <pedrohenriquecamposmendes@hotmail.com>
2023-10-27 17:57:25 -0300

    chore: init ignite project 02

---
+++ node/projeto02/src/server.ts
@@ -0,0 +1,15 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/", () => {
+  return;
+});
+
+app
+  .listen({
+    port: 3000,
+  })
+  .then(() => {
+    console.log("[INFO]: Server is running on port 3000");
+  });
